32409b3d682ce7621791eaf810b96d41bd51023c
Eder Martins <contato@eder.poa.br>
2021-02-08 20:01:27 -0300

    Change Resources to "copy"

--- Package.swift
+++ Package.swift
@@ -19,6 +19,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AcessoBio",
-            resources: [.process("Resources")]),
+            resources: [.copy("Resources")]),
     ]
 )
